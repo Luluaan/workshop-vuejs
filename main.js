4a674688e9308367393d8d97b5709fc20c6236fb
Bozier Luan <luan.bozier@he-arc.ch>
2024-10-31 09:30:52 +0100

    finish 5

--- main.js
+++ main.js
@@ -8,6 +8,42 @@ const app = Vue.createApp({
         inStock: true,
         stock: 5,
         onSale: false,
+        details: ['Doux', 'Harmonieux'],
+        carouselImages: [
+            {
+              id: 1,
+              text: 'Capsule 1',
+            },
+            {
+              id: 2,
+              text: 'Capsule 2',
+            },
+            {
+              id: 3,
+              text: 'Tasse',
+            },
+            {
+              id: 4,
+              text: 'Paquet',
+            }
+          ],
+          paquets: [
+            {
+              id: 1,
+              quantity: 2,
+              price: 5
+            },
+            {
+              id: 2,
+              quantity: 10,
+              price: 34
+            },
+            {
+              id: 3,
+              quantity: 1,
+              price: 3
+            }
+          ],
       };
     },
   });
