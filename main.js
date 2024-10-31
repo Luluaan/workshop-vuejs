de17b8c8de811c001b0d008f9d31daa4fe09d0ba
Bozier Luan <luan.bozier@he-arc.ch>
2024-10-31 09:10:44 +0100

    finish 2

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data: function () {
+      return {
+        title: 'Achat de café Nespresso',
+        description: "C'est pour acheter du café du coup"
+      };
+    },
+  });
